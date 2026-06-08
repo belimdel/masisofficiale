e209d4cdfc711d84e1bc70bc69b65d711d8b7706
belimdel <hmher688@gmail.com>
2026-06-08 15:16:17 +0200

    Mise à jour du projet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://masis-paysagiste.fr',
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/services/services'),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
